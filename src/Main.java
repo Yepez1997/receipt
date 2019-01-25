ba4ad95385b0db438ea91399d32dcb7803d0c5de
Aureliano Yepez <ayepez2020@berkeley.edu>
2019-01-24 18:50:25 -0800

    reading files

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 /* Driver Class */
 public class Main {
     public static void Main(String args[]) {
 
-    // want to read input from files 
+    // TODO: READ INPUT 
+    // run main + /path to directory 
+     try {
+         // try to open the file 
+        FileInputStream fd = new FileInputStream(args[0]);
+        Scanner fileScanner = new Scanner(fd); 
+        System.out.println("File has been opened");
+
+        // open and get every line 
+        while(fileScanner.hasNextLine()) {
+            String oneLine = fileScanner.nextLine(); 
+            System.out.println(oneLine);
+        }
+
+
+
+     }
+     // file not found 
+     catch(FileNotFoundException fnfe) {
+         System.out.println("File was not found");
+     }
     }
 }
